f8305ed04e6e13052d51eaf0648318bfb8257cad
LeandrodaSilva <ld_silva13@hotmail.com>
2019-12-01 18:17:10 -0300

    Alterado botão de login com Google Auth 2

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -6,6 +6,7 @@ import { Input, Form } from '@rocketseat/unform';
 import { DefaultButton } from '~/components/Buttons';
 import { signInRequest } from '~/store/modules/auth/actions';
 import logo from '~/assets/logo.png';
+import GSignIn2 from '~/pages/GSignIn2';
 
 const schema = Yup.object().shape({
   email: Yup.string()
@@ -28,7 +29,7 @@ export default function SignIn() {
       <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input name="password" type="password" placeholder="Sua senha" />
-        <div className="g-signin2" data-onsuccess="onSignIn" />
+        <GSignIn2 />
         <DefaultButton type="submit">
           {loading ? `carregando...` : `Acessar`}
         </DefaultButton>

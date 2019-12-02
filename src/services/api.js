6adbca5dbb2d31ae483679fa3bf812651d556731
LeandrodaSilva <ld_silva13@hotmail.com>
2019-12-01 21:01:44 -0300

    adicionado .envExample

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
-import strings from '~/strings';
+// import strings from '~/strings';
 
 const api = axios.create({
-  baseURL: strings.baseUrl,
+  // baseURL: strings.baseUrl,
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;

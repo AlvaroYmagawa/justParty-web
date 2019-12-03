3989f663eb597770150d0f1f8b9e40a6616fa628
Leandro da Silva <ld_silva13@hotmail.com>
2019-12-03 15:13:48 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import strings from '~/strings';
 
 const api = axios.create({
-  baseURL: strings.baseUrl,
-  // baseURL: process.env.REACT_APP_API_URL,
+//   baseURL: strings.baseUrl,
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
